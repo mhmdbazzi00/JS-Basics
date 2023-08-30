e972def14261e56115e6ca058e3de8bcaf445b7d
Mohammad Bazzi <135434578+mhmdbazzi00@users.noreply.github.com>
2023-08-30 22:30:21 +0300

    Step_2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,7 @@
+function toggle_visibility(div) {
+    var e = document.getElementById('texte');
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
