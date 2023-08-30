2d4dd52a073f809d5d48be1a4b5bb444589910c1
Mohammad Bazzi <135434578+mhmdbazzi00@users.noreply.github.com>
2023-08-30 12:52:59 +0300

    multiple operations

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,8 @@
+let shoeSize = prompt("Shoe Size: ");
+let birthYear = prompt("Birth Year: ");
+let number = shoeSize *2 + 5 * 50 - birthYear + 1766
+function Multiplication (number){
+    return number
+
+}
+alert(number);
