f94656fc597ce9592959a67d935502dd5d1b8de7
Mohammad Bazzi <135434578+mhmdbazzi00@users.noreply.github.com>
2023-08-30 14:36:51 +0300

    image

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -2,7 +2,7 @@ let shoeSize = prompt("Shoe Size: ");
 let birthYear = prompt("Birth Year: ");
 let number = shoeSize *2 + 5 * 50 - birthYear + 1766
 function Multiplication (number){
-    return number
+    return number;
 
 }
 alert(number);
---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,5 @@
+function change(){
+    const image = document.querySelector('img');
+    image.src = 'images/image1_2.jpg';
+
+}
