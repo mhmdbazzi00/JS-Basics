2baf1aac6aa04f21601b9a9f9123b247e8259139
Mohammad Bazzi <135434578+mhmdbazzi00@users.noreply.github.com>
2023-08-30 20:35:47 +0300

    5 images

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,8 @@
+function all(){
+    const image = document.querySelectorAll('img');
+    image.src = 'images/image1.jpg';
+    image.src = 'images/image2.jpg';
+    image.src = 'images/image3.jpg';
+    image.src = 'images/image4.jpg';
+    image.src = 'images/image5.jpg';
+}
