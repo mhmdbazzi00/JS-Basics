1926368d1ff43e005e0ee19340bd1bbd4bd88c7c
Mohammad Bazzi <135434578+mhmdbazzi00@users.noreply.github.com>
2023-08-31 15:20:14 +0300

    Step 2

--- javascript-basics-03/step 2/Step_2.js
+++ javascript-basics-03/step 2/Step_2.js
@@ -1,7 +1,18 @@
-function toggle_visibility() {
-    var e = document.getElementById('texte');
-    if(e.style.display == 'block')
-       e.style.display = 'none';
-    else
-       e.style.display = 'block';
-}
+const btns=document.querySelectorAll("a");
+const div=document.querySelector("#texte");
+
+
+btns.forEach(btn=>{
+    if(btn.id==="show"){
+        btn.addEventListener("click",(event)=>{
+            event.preventDefault();
+            div.style.display="block";
+        })
+    }else{
+        btn.addEventListener("click",(event)=>{
+            event.preventDefault();
+            div.style.display="none";
+        })
+
+    }
+})
